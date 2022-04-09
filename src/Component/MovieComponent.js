150965e69b1f0d3012bd0a0aa0757a49e2f3d5af
Rishab-Rana <rishabrana228@gmail.com>
2022-04-09 08:24:26 +0530

    done with all work

--- src/Component/MovieComponent.js
+++ src/Component/MovieComponent.js
@@ -1,54 +1,58 @@
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
 const MovieContainer = styled.div`
-display: flex;
-flex-direction: column;
-padding:10px;
-width:280px;
-box-shadow: 0 3px 10px 0 #aaa;
-cursor:pointer;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  width: 280px;
+  box-shadow: 0 3px 10px 0 #aaa;
+  cursor: pointer;
 `;
-
-const CoverImage= styled.img`
-height:362px;
-object-fit:cover;
+const CoverImage = styled.img`
+  object-fit: cover;
+  height: 362px;
 `;
-
-const MovieName = styled.div`
-font-size:18px;
-font-weight:600;
-color:black;
-margin: 15px 0;
-white-space:nowrap;
-text-overflow:ellipsis;
-overflow:hidden;
+const MovieName = styled.span`
+  font-size: 18px;
+  font-weight: 600;
+  color: black;
+  margin: 15px 0;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
-
 const InfoColumn = styled.div`
-display: flex;
-flex-direction: row;
-justify-content: space-between;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `;
-
-const MovieInfo = styled.div`
-font-size:16px;
-font-weight:500;
-color:black;
-text-transform:capitalize;
+const MovieInfo = styled.span`
+  font-size: 16px;
+  font-weight: 500;
+  color: black;
+  white-space: nowrap;
+  overflow: hidden;
+  text-transform: capitalize;
+  text-overflow: ellipsis;
 `;
-
 const MovieComponent = (props) => {
+  const { Title, Year, imdbID, Type, Poster } = props.movie;
 
-    return (
-        <MovieContainer>
-            <CoverImage src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg" />
-             <MovieName>Rishab</MovieName>
-             <InfoColumn>
-                 <MovieInfo>Year: 2012</MovieInfo>
-                 <MovieInfo>Type: Movie</MovieInfo>
-             </InfoColumn>
-             </MovieContainer>
-    )
-}
-
+  return (
+    <MovieContainer
+      onClick={() => {
+        props.onMovieSelect(imdbID);
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      }}
+    >
+      <CoverImage src={Poster} alt={Title} />
+      <MovieName>{Title}</MovieName>
+      <InfoColumn>
+        <MovieInfo>Year : {Year}</MovieInfo>
+        <MovieInfo>Type : {Type}</MovieInfo>
+      </InfoColumn>
+    </MovieContainer>
+  );
+};
 export default MovieComponent;
