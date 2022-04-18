cf052894a234ef3d11900ce215add765d624ccfa
Lucifer <rishabrana228@gmail.com>
2022-04-19 00:50:16 +0530

    added hover effect on movie container and adjusted the width of feature movie container

--- src/Component/NewMovie.js
+++ src/Component/NewMovie.js
@@ -42,17 +42,105 @@ position:relative;
   }
 `;
 
-function NewMovieList(){
-    return(
+const FeatureMovies = styled.div`
+display: flex;
+flex-direction: row;
+width:1100px;
+overflow-y:hidden;
+overflow-x:scroll;
+-webkit-overflow-scrolling: touch;
+::-webkit-scrollbar {
+    display: none;
+}
+
+`;
+
+const FeaturesMoviesContainer = styled.div`
+display: flex;
+padding:10px;
+`;
+const MovieContainer = styled.div`
+width: 150px;
+height: 150px;
+overflow: hidden;
+position: relative;
+transition: all 1s;
+&:hover{
+    width: 300px;
+}
+`;
+
+const MovieCoverImanger = styled.img`
+width: 100%;
+height: 100%;
+background-position: center;
+background-size: cover;
+background-repeat: no-repeat;
+transition: all 2s;
+object-fit:cover;
+&:hover{
+    width: 300px;
+    opacity: 0.4;
+}
+
+`
+
+function NewMovieList() {
+    return (
         <NewMovieListContainer>
-        <HeadingText>Watch New Movie & TV Trailers</HeadingText>
-        <MovieCategory>
-            <MovieCategoryList>TRADING TRAILER</MovieCategoryList>
-            <MovieCategoryList>MOST ANTICIPATED</MovieCategoryList>
-            <MovieCategoryList>MOST POPULAR</MovieCategoryList>
-            <MovieCategoryList>RECENTLY ADDED</MovieCategoryList>
-        </MovieCategory>
-        
+            <HeadingText>Watch New Movie & TV Trailers
+            </HeadingText>
+            <MovieCategory>
+                <MovieCategoryList>TRADING TRAILER</MovieCategoryList>
+                <MovieCategoryList>MOST ANTICIPATED</MovieCategoryList>
+                <MovieCategoryList>MOST POPULAR</MovieCategoryList>
+                <MovieCategoryList>RECENTLY ADDED</MovieCategoryList>
+            </MovieCategory>
+            <FeatureMovies>
+                <FeaturesMoviesContainer>
+                    <MovieContainer>
+                    <MovieCoverImanger src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg" />
+                    </MovieContainer>
+                </FeaturesMoviesContainer>
+                <FeaturesMoviesContainer>
+                    <MovieContainer>
+                    <MovieCoverImanger src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg" />
+                    </MovieContainer>
+                </FeaturesMoviesContainer>
+                <FeaturesMoviesContainer>
+                    <MovieContainer>
+                    <MovieCoverImanger src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg" />
+                    </MovieContainer>
+                </FeaturesMoviesContainer><FeaturesMoviesContainer>
+                    <MovieContainer>
+                    <MovieCoverImanger src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg" />
+                    </MovieContainer>
+                </FeaturesMoviesContainer><FeaturesMoviesContainer>
+                    <MovieContainer>
+                    <MovieCoverImanger src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg" />
+                    </MovieContainer>
+                </FeaturesMoviesContainer><FeaturesMoviesContainer>
+                    <MovieContainer>
+                    <MovieCoverImanger src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg" />
+                    </MovieContainer>
+                </FeaturesMoviesContainer><FeaturesMoviesContainer>
+                    <MovieContainer>
+                    <MovieCoverImanger src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg" />
+                    </MovieContainer>
+                </FeaturesMoviesContainer><FeaturesMoviesContainer>
+                    <MovieContainer>
+                    <MovieCoverImanger src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg" />
+                    </MovieContainer>
+                </FeaturesMoviesContainer><FeaturesMoviesContainer>
+                    <MovieContainer>
+                    <MovieCoverImanger src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg" />
+                    </MovieContainer>
+                </FeaturesMoviesContainer><FeaturesMoviesContainer>
+                    <MovieContainer>
+                    <MovieCoverImanger src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg" />
+                    </MovieContainer>
+                </FeaturesMoviesContainer>
+            </FeatureMovies>
         </NewMovieListContainer>
     );
 }
